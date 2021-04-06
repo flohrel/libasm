681983f2ac3efcbc06f63fb6fe96c32a9682b748
flohrel <flohrel@student.42.fr>
2021-04-06 17:47:49 +0200

    buidi

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,9 +1,9 @@
 ; Determines the length of a C-style NUL-terminated string
 ;
-; Inputs:	RDI = address of string copy
-;			RSI = address of source string
-; Outputs:	RAX = address of string copy
-; Clobbers:	<none>
+; Inputs:	RDI = address of first string
+;			RSI = address of second string
+; Outputs:	RAX = difference between strings (-1/0/1 if lt/eq/gt)
+; Clobbers:	CL, flags
 
 	global	ft_strcmp
 
@@ -17,23 +17,23 @@ ft_strcmp:
 .L2:
 	mov		cl, BYTE [rdi+rax]
 	cmp		cl, 0
-	jz		compare
+	jz		.compare
 	cmp		BYTE [rsi+rax], cl
 	je		.L1
 
-compare:
+.compare:
 	cmp		BYTE [rsi+rax], cl
-	ja		greater
-	jb		lower
+	ja		.greater
+	jb		.lower
 
-equal:
+.equal:
 	xor		rax, rax
 	ret
 
-greater:
+.greater:
 	mov		rax, -1
 	ret
 
-lower:
+.lower:
 	mov		rax, 1
 	ret
