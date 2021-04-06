1ccdc9a0de8548dae77fe1f2030c2827a2dd8a94
flohrel <flohrel@student.42.fr>
2021-04-06 17:45:13 +0200

    strcmp fonctionnel, mais un peu nul

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -9,18 +9,31 @@
 
 ft_strcmp:
 	xor		rax, rax
-	jmp		loop
+	jmp		.L2
 
-loop:
+.L1:
 	inc		rax
+
+.L2:
 	mov		cl, BYTE [rdi+rax]
 	cmp		cl, 0
-	jz		exit
+	jz		compare
 	cmp		BYTE [rsi+rax], cl
-	jnz		exit
-	jmp		loop
+	je		.L1
 
-exit:
-	sub		cl, BYTE [rsi+rax]
-	mov		al, cl
+compare:
+	cmp		BYTE [rsi+rax], cl
+	ja		greater
+	jb		lower
+
+equal:
+	xor		rax, rax
+	ret
+
+greater:
+	mov		rax, -1
+	ret
+
+lower:
+	mov		rax, 1
 	ret
