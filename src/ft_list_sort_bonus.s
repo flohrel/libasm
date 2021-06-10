d8231afaf259de8b759f5a8d5f85760c518cce9f
flohrel <flohrel@student.42.fr>
2021-06-10 18:22:00 +0200

    new libft

--- src/ft_list_sort_bonus.s
+++ src/ft_list_sort_bonus.s
@@ -6,10 +6,11 @@
 #    By: flohrel <flohrel@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/05/02 06:43:57 by flohrel           #+#    #+#              #
-#    Updated: 2021/06/10 13:13:18 by flohrel          ###   ########.fr        #
+#    Updated: 2021/06/10 15:41:26 by flohrel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+	extern	ft_list_size
 	global	ft_list_sort
 
 ft_list_sort:
@@ -17,17 +18,25 @@ ft_list_sort:
 	mov		rbp, rsp			;    stack
 	test	rdi, rdi			; test list not a NULL ptr
 	jz		.exit
-	mov		r8, [rdi]			; pointer to begining of list in r8
-	push	rdi					; save start of list in stack
+#	mov		r8, [rdi]			; pointer to begining of list in r8
+#	push	rdi					; save start of list in stack
 	test	rsi, rsi			; test cmp func not a NULL ptr
 	jz		.exit
-	mov		rdx, rsi			; ptr to cmp func in rdx
-	push	rsi					; save sort func ptr in stack
-.loop:
-	mov		r9, [r8 + 8]		; get next lst
-	test	r9, r9				; test if NULL
-	jz		.exit
-	
+#	mov		rdx, rsi			; ptr to cmp func in rdx
+#	push	rsi					; save sort func ptr in stack
+	call	merge_sort
+
+merge_sort:
+	call	ft_list_size
+	cmp		rax, 1
+	jz		.return
+	idiv	rax, 2
+	mov		rdx, [rdi+8*rax]
+	mov		QWORD [rdi+8*rax], 0
+.return:
+	mov		rax, rdi
+	ret
+
 .exit:
 	leave
 	ret
